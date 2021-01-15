53d9392386512f9755b902c30a3c356bb00957e9
sai_manoj_s <smsmanoj91@gmail.com>
2021-01-15 16:35:57 +0530

    port set

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = 3000;
+var port_number = server.listen(process.env.PORT || 3000);
 
 // Set public folder as root
 app.use(express.static('public'));
@@ -12,6 +12,6 @@ app.use('/scripts', express.static(`${__dirname}/node_modules/`));
 // Redirect all traffic to index.html
 app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
-app.listen(port, () => {
+app.listen(port_number, () => {
   console.info('listening on %d', port);
 });
